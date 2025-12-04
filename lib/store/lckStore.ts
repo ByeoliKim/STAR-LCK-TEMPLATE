6723ce0f8c6e485498d7c6c0535f76b0da0668d0
ByeoliKim <213069@naver.com>
2025-12-05 00:00:40 +0900

    feat: 로컬스토리지 저장

--- lib/store/lckStore.ts
+++ lib/store/lckStore.ts
@@ -4,11 +4,14 @@
 // 등등 전역으로 관리하여 여러 컴포넌트에서 공유할 수 있다.
 
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import type { LolRole } from "@/lib/config/teams";
 
 // 전역 상태 shape 정의
 
-// 찜한 선수 식별용 타입 (팀 + 선수 id)
+// 찜한 선수 식별용 타입
+// teamSlug : 어떤 팀인가
+// playerId : 팀 안에서의 선수 id
 export type FavoritePlayerRef = {
   teamSlug: string;
   playerId: string;
@@ -19,39 +22,60 @@ type LckState = {
   selectedRole: LolRole;
   setSelectedRole: (role: LolRole) => void;
 
+  // 교차 텍스트 스크롤 모션 속도 제어
   scrollVelocity: number;
   setScrollVelocity: (velocity: number) => void;
 
   // 전역 즐겨찾기(찜) 목록
   favorites: FavoritePlayerRef[];
   toggleFavorite: (ref: FavoritePlayerRef) => void;
+
+  // 전역 사이드바 on/off 상태
+  isFavoritesSidebarOpen: boolean;
+  setFavoritesSidebarOpen: (open: boolean) => void;
 };
 
 // 스토어 생성
-export const useLckStore = create<LckState>((set, get) => ({
-  // 디폴트 포지션은 MID 으로 설정
-  selectedRole: "MID",
-  setSelectedRole: (role) => set({ selectedRole: role }),
 
-  // 텍스트 스크롤 속도 기본값
-  scrollVelocity: 50,
-  setScrollVelocity: (velocity) => set({ scrollVelocity: velocity }),
+export const useLckStore = create<LckState>()(
+  persist(
+    (set, get) => ({
+      // 디폴트 포지션은 MID 으로 설정
+      selectedRole: "MID",
+      setSelectedRole: (role) => set({ selectedRole: role }),
 
-  favorites: [],
-  toggleFavorite: (ref) => {
-    const { favorites } = get();
-    const exists = favorites.some(
-      (f) => f.teamSlug === ref.teamSlug && f.playerId === ref.playerId
-    );
-    // 토글
-    if (exists) {
-      set({
-        favorites: favorites.filter(
-          (f) => !(f.teamSlug === ref.teamSlug && f.playerId === ref.playerId)
-        ),
-      });
-    } else {
-      set({ favorites: [...favorites, ref] });
+      // 텍스트 스크롤 속도 기본값
+      scrollVelocity: 50,
+      setScrollVelocity: (velocity) => set({ scrollVelocity: velocity }),
+
+      favorites: [],
+      toggleFavorite: (ref) => {
+        const { favorites } = get();
+        const exists = favorites.some(
+          (f) => f.teamSlug === ref.teamSlug && f.playerId === ref.playerId
+        );
+        // 토글
+        if (exists) {
+          set({
+            favorites: favorites.filter(
+              (f) =>
+                !(f.teamSlug === ref.teamSlug && f.playerId === ref.playerId)
+            ),
+          });
+        } else {
+          set({ favorites: [...favorites, ref] });
+        }
+      },
+
+      isFavoritesSidebarOpen: true, // 디폴트 상태는 ON
+      setFavoritesSidebarOpen: (open) => set({ isFavoritesSidebarOpen: open }),
+    }),
+    {
+      name: "lck-store",
+      partialize: (state) => ({
+        favorites: state.favorites,
+        isFavoritesSidebarOpen: state.isFavoritesSidebarOpen,
+      }),
     }
-  },
-}));
+  )
+);
