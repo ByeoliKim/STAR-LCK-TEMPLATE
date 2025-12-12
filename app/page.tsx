4c522a325a25e8ca1ddfe03886b81c374735b8da
ByeoliKim <213069@naver.com>
2025-12-12 15:31:43 +0900

    remote test

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
         <div className="">
           <div className="p-9 lg:p-0 relative w-full max-w-5xl flex flex-col min-h-screen items-center justify-center m-auto overflow-hidden ">
             <h2 className="flex flex-col md:flex-row lg:flex-row w-full mb-30 md:mb-8 lg:mb-8 pb-2 text-left text-3xl md:text-4xl lg:text-5xl font-black tracking-tighter border-b">
-              STAR TEMPLATE..
+              STAR TEMPLATE.
               <strong className="md:ml-auto lg:ml-auto">LCK Ver.</strong>
             </h2>
             <section className="relative translate-y-[-8em] md:translate-y-[-3em] lg:translate-y-[-4em] z-10">
